8faee03a9d279095023a9486ab091e7665543e07
aniketnakhate <aniketpn123@gmail.com>
2026-02-28 09:40:56 +0530

    new version

--- mid/smartNotesApp/script.js
+++ mid/smartNotesApp/script.js
@@ -1,35 +1,12 @@
-const newNote =document.querySelector('.new-note');
-const notesList =document.querySelector('.notes-list');
-const deleteImg =document.querySelector('.delete-img');
-const notes =document.querySelectorAll('.note');
+// notes create data store
 
-function getdata(){
-    notesList.innerHTML = localStorage.getItem('note');
-}
-function loaddata(){
-    localStorage.setItem('note', notesList.innerHTML);
-}
-getdata();
-newNote.addEventListener('click',()=>{
-       let div = document.createElement('div');
-       div.className = 'note';
-        div.setAttribute('contenteditable',"true");
-        let i = document.createElement('i');
-        i.textContent='notes';
-        let img =document.createElement('img');
-        img.src="images/delete.png";
-        img.alt = 'img';
-        img.className='delete-img';
-    notesList.appendChild(div).appendChild(i);
-    notesList.appendChild(div).appendChild(img);
-
-    loaddata();
-})
-
-notesList.addEventListener('click',(e)=>{
-    if(e.target.tagName === 'IMG')
-    {
-        e.target.parentElement.remove(); 
-        loaddata();
-    }
+createNote.addEventListener('click',()=>{
+    let div = document.createElement('div');
+    let i = document.createElement('i');
+    let img = document.createElement('img');
+    div.setAttribute("contenteditable" ,"true");
+    div.className="note";
+    img.src ="images/delete.png";
+    img.alt ="img";
+    .appendChild(div).appendChild(i).appendChild(img);
 })
