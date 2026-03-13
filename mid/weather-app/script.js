1efdb2ecb6ebe522403cf7cd39d76df51060d084
aniketnakhate <aniketpn123@gmail.com>
2026-03-13 23:14:18 +0530

    fetch api

--- mid/weather-app/script.js
+++ mid/weather-app/script.js
@@ -1,2 +1,18 @@
-let promise = new Promise("");
+async function getWeather() {
+  const key = "ebf515992246836a3970c14871608e63";
+  const city = document.getElementById("city").value;
+  const display = document.querySelector(".display");
 
+  const url = `    https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
+
+  try {
+    let response = await fetch(url);
+    let data = await response.json();
+
+    display.innerHTML = `${data.main.temp}%`;
+  } catch {
+    console.log(Error, "error");
+  }
+}
+
+getWeather();
