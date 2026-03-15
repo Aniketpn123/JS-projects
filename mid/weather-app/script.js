37f23bb23197e95d925704b2c17e31167e7b1210
aniketnakhate <aniketpn123@gmail.com>
2026-03-15 23:12:52 +0530

    try catch and fetch api

--- mid/weather-app/script.js
+++ mid/weather-app/script.js
@@ -1,18 +1,32 @@
-async function getWeather() {
-  const key = "ebf515992246836a3970c14871608e63";
-  const city = document.getElementById("city").value;
-  const display = document.querySelector(".display");
-
-  const url = `    https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
+let display = document.querySelector(".display");
+getData();
+async function weatherApp(params) {
+  let key = "ebf515992246836a3970c14871608e63";
+  let city = document.getElementById("city").value.trim();
+  display.innerHTML = "Loading weather...";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${key}`;
 
   try {
     let response = await fetch(url);
     let data = await response.json();
 
-    display.innerHTML = `${data.main.temp}%`;
-  } catch {
-    console.log(Error, "error");
+    display.innerHTML = `<p class="cityName">${data.name}</p>
+          <p class="temp">${data.main.temp} °C</p>`;
+  }catch (err) {
+
+  if (err.message.includes("")) {
+    display.innerHTML = "City not found";
+  } else {
+    display.innerHTML = "Network error";
   }
+
+}
+  saveData();
 }
 
-getWeather();
+function saveData() {
+  localStorage.setItem("data", display.innerHTML);
+}
+function getData() {
+  display.innerHTML = localStorage.getItem("data");
+}
